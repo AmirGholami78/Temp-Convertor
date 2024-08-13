d931f602c630a04a1348e002b28e6862f5e25757
Amirgh <144719295+AmirGholami78@users.noreply.github.com>
2024-08-13 23:08:46 +0330

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+﻿let $ = document
+
+let firstValue = $.querySelector('.C')
+let secondValue = $.querySelector('.F')
+let converter = $.querySelector('#converter')
+let result = $.querySelector('.result')
+let convertBtn = $.querySelector('.convertButton')
+let resetBtn = $.querySelector('.resetButton')
+let changeBtn = $.querySelector('.changeButton')
+
+
+
+function convert () {
+    if (converter.value == '') {
+        result.innerHTML = 'Insert Valid Value'
+        result.style.color = 'red'
+    } else {
+        if (firstValue.innerHTML == '°C') {
+            result.innerHTML = (converter.value * 2 + 30) + '°F'
+            result.style.color = 'green'
+            
+        }else {
+            result.innerHTML = ((converter.value  - 30)/2) + '°C'
+            result.style.color = 'green'
+        }
+    }
+    
+}
+function reset () {
+    result.innerHTML = ''
+    converter.value = ''
+}
+function change () {
+
+if (firstValue.innerHTML == '°C') {
+    firstValue.innerHTML = '°F'
+    secondValue.innerHTML = '°C'
+    converter.setAttribute('placeholder' , '°F')
+} else {
+    firstValue.innerHTML = '°C'
+    secondValue.innerHTML = '°F'
+    converter.setAttribute('placeholder' , '°C')
+}
+}
+
+
+convertBtn.addEventListener('click' , convert)
+resetBtn.addEventListener('click' , reset)
+changeBtn.addEventListener('click' , change)
